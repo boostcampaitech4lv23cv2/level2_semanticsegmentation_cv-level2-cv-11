9981221a25cc5572312fc5f3ef3850789f89e200
teajun <ta1231@ajou.ac.kr>
2022-12-27 16:53:12 +0000

    feat: on fix

--- mmsegmentation/mmseg/datasets/trash.py
+++ mmsegmentation/mmseg/datasets/trash.py
@@ -1,5 +1,6 @@
 from .custom import CustomDataset
-from .builder import DATASETS
+from .builder import DATASETS, PIPELINES
+from pycocotools.coco import COCO
 
 @DATASETS.register_module()
 class TrashDataset(CustomDataset):
@@ -39,10 +40,52 @@ class TrashDataset(CustomDataset):
         classes=None,
         palette=None,
         gt_seg_map_loader_cfg=None,
-        file_client_args=dict(backend='disk')
-    ):
-    self.gt_seg_map_loader = CustomLoadAnnotations(coco_json_path)
+        file_client_args=dict(backend='disk'),
+    ): 
+        self.gt_seg_map_loader = CustomLoadAnnotations(coco_json_path)
+        self.img_infos=gt_seg_map_loader.load_annotations()
+    
 
+    
+
+
+
+class CustomLoadAnnotations(object):
+    def __init__(
+        self,
+        coco_json_path,
+        reduce_zero_label=False,
+        file_client_args=dict(backend='disk'),
+        imdecode_backend='pillow'):
+
+        self.coco = COCO(coco_json_path)
+        self.reduce_zero_label = reduce_zero_label
+        self.file_client_args = file_client_args.copy()
+        self.file_client = None
+        self.imdecode_backend = imdecode_backend
+
+    def __call__(self, results):
+        coco_ind = results['ann_info']['coco_image_id']
+        image_info = self.coco.loadImgs(coco_ind)[0]
+        ann_inds = self.coco.getAnnIds(coco_ind)
+        anns = self.coco.loadAnns(ann_inds)
+        anns = list(sorted(anns, key=lambda x: -x['area']))
+
+        gt_sematic_seg = np.zeros((image_info['heights'], image_info['width']))
+        for ann in anns:
+            gt_semantic_seg[self.coco.annToMask(ann)==1] = ann['category_id']
+        gt_semantic_seg = gt_semantic_seg.astype(np.int64)
+
+        if results.get('label_map', None) is not None:
+            for old_id, new_id in results['label_map'].items():
+                gt_semantic_seg[gt_semantic_seg==old_id] = new_id
+        if self.reduce_zero_label:
+            gt_semantic_seg[gt_semantic_seg==0] = 255
+            gt_semantic_seg = gt_semantic_seg - 1
+            gt_semantic_seg[gt_semantic_seg == 254] = 255
+        results['gt_semantic_seg'] = gt_semantic_seg
+        results['seg_fields'].append('gt_semantic_seg')
+        return results
 
     def load_annotations(self):
         img_infos = []
@@ -62,9 +105,3 @@ class TrashDataset(CustomDataset):
         result['img_prefix'] = self.img_dir
         if self.custom_classes:
             results['label_map'] = self.label_map
-
-    
-    
-
-
-    
