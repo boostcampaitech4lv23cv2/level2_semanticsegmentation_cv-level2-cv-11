33b2bbdbc4a64c669220ff868de9c42625be2f0a
jerry-ryu <fbrjs15@naver.com>
2022-12-31 11:55:37 +0000

    refactoring: divide test, valid pipeline
    referenced issue: #17

--- mmsegmentation/configs/_teajun_/_base_/datasets/trashcustom.py
+++ mmsegmentation/configs/_teajun_/_base_/datasets/trashcustom.py
@@ -17,12 +17,27 @@ train_pipeline = [
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
+
+valid_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512, 512),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
         img_scale=(512, 512),
-        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -46,7 +61,7 @@ data = dict(
         is_valid=True,
         img_dir='/opt/ml/input/data',
         coco_json_path='/opt/ml/input/data/val.json',
-        pipeline=test_pipeline),
+        pipeline=valid_pipeline),
     test=dict(
         type=dataset_type,
         is_valid=True,
