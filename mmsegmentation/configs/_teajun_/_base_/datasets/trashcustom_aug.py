65aedd77098740b73c459077c6603931e5e8b725
jerry-ryu <fbrjs15@naver.com>
2023-01-02 08:11:48 +0000

    feat: add augmentation config and dataset

--- mmsegmentation/configs/_teajun_/_base_/datasets/trashcustom_aug.py
+++ mmsegmentation/configs/_teajun_/_base_/datasets/trashcustom_aug.py
@@ -6,58 +6,6 @@ img_norm_cfg = dict(
 crop_size = (512, 512)
 
 # train pipeline에 적용되는 Albu transforms
-# aug 2
-albu_train_transforms =[
-    #aug 1 = all
-            # dict(
-            #     type='ShiftScaleRotate', # 돌리고 밀기
-            #     shift_limit=0.0625,
-            #     scale_limit=0,
-            #     rotate_limit=30,
-            #     p=0.5,
-            # ),
-            dict(
-                type='OneOf', # 이 중에 하나만 적용
-                transforms=[
-                    dict(type='ElasticTransform', p=1.0), # 이미지를 찌그러지게 만든다. # https://www.researchgate.net/figure/Grid-distortion-and-elastic-transform-applied-to-a-medical-image_fig4_327742409
-                    dict(type='Perspective', p=1.0), # 4점 투시 변환 # https://minimin2.tistory.com/135
-                    dict(type='PiecewiseAffine', p=1.0), # 이미지를 찌그러지게 만든다 # https://scikit-image.org/docs/stable/auto_examples/transform/plot_piecewise_affine.html
-                ],
-                p=0.3),
-            # dict(
-            #     type='Affine', # 아핀 변환
-            #   p=0.3  
-            # ),
-            # dict(
-            #     type='OneOf',
-            #     transforms=[
-            #         dict(type='RGBShift', r_shift_limit=20, g_shift_limit=20,b_shift_limit=20,always_apply=False,p=1.0), # rgb 값 변경
-            #         dict(type='ChannelShuffle', p=1.0) # rgb 값 무작위 재배열
-            #     ],
-            #     p=0.5),
-            dict(
-                type='RandomBrightnessContrast', # 밝기와 대비를 무작위로 변경
-                brightness_limit=0.1,
-                contrast_limit=0.15,
-                p=0.5),
-            dict(
-                type='HueSaturationValue',  #색조, 채도 및 값을 무작위로 변경
-                hue_shift_limit=15,
-                sat_shift_limit=25,
-                val_shift_limit=10,
-                p=0.5),
-            # dict(type='GaussNoise', p=0.3), # 가우시안 노이즈를 추가
-            dict(type='CLAHE', p=0.5), # https://m.blog.naver.com/samsjang/220543360864
-            # dict(
-            #     type='OneOf',
-            #     transforms=[ # 블러 적용
-            #         dict(type='Blur', p=1.0),
-            #         dict(type='GaussianBlur', p=1.0),
-            #         dict(type='MedianBlur', blur_limit=5, p=1.0)
-            #     ],
-            #     p=0.3),
-        ]
-
 #aug3
 albu_train_transforms_aug3 = [
     dict(type='RandomBrightnessContrast', brightness_limit=0.1, contrast_limit=0.15, p=0.5),
