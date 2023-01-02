c77df919a6132ad51db1d5314cbecf2015f54a43
teajun <ta1231@ajou.ac.kr>
2023-01-02 02:01:27 +0000

    feat: knet실험 추가 및 convnext resume파일 추가

---
+++ mmsegmentation/configs/_teajun_/knet/knet_s3_upernet_swin-l_8x2_512x512_adamw_80k_ade20k_resume.py
@@ -0,0 +1,23 @@
+_base_ = 'knet_s3_upernet_swin-t_8x2_512x512_adamw_80k_ade20k.py'
+
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_large_patch4_window7_224_22k_20220308-d5bdebaf.pth'  # noqa
+# model settings
+model = dict(
+    pretrained=checkpoint_file,
+    backbone=dict(
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=7,
+        use_abs_pos_embed=False,
+        drop_path_rate=0.3,
+        patch_norm=True),
+    decode_head=dict(
+        kernel_generate_head=dict(in_channels=[192, 384, 768, 1536])),
+    auxiliary_head=dict(in_channels=768))
+# In K-Net implementation we use batch size 2 per GPU as default
+data = dict(samples_per_gpu=2, workers_per_gpu=2)
+resume_from = '/opt/ml/mmsegmentation/work_dirs/knet_s3_upernet_swin-l_8x2_512x512_adamw_80k_ade20k/iter_10000.pth'
+runner = dict(type='IterBasedRunner', max_iters=20000)
+checkpoint_config = dict(by_epoch=False, interval=2000)
+evaluation = dict(interval=1000, metric='mIoU', pre_eval=True)
